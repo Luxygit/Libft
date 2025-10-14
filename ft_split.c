5aa91aa71d5a00b2f123420fc6af8a8cd243a2e4
Diego Vargas <divaro_@hotmail.com>
2025-10-14 13:33:13 +0200

    meh

--- ft_split.c
+++ ft_split.c
@@ -6,79 +6,101 @@
 /*   By: dievarga <dievarga@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 14:21:31 by dievarga          #+#    #+#             */
-/*   Updated: 2025/10/09 12:23:53 by dievarga         ###   ########.fr       */
+/*   Updated: 2025/10/12 13:46:45 by dievarga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static int	ft_count(const char *s, char c)
+static int	ft_count(char const *s, char c)
 {
-	int	count;
-	int	contained;
-
-	count = 0;
-	contained = 0;
+	int	n;
+	
+	n = 0;
+	if (!*s)
+		return (0);
 	while (*s)
 	{
-		if (*s != c && !contained)
+		if (*s == c)
 		{
-			contained = 1;
-			count++;
+			n++;
+			while (*s == c)
+				s++;
 		}
-		else if (*s == c)
-			contained = 0;
-		s++;
+		else
+			s++;
 	}
-	return (count);
+	n++;
+	return (n);
 }
 
-static char	*ft_dup(const char *s, int len)
+static void	*ft_freeall(char **mem, size_t i)
 {
-	char	*word;
-	int		i;
+	size_t	j;
+
+	j = 0;
+	while (j < i)
+	{
+		free(mem[j]);
+		j++;
+	}
+	free(mem);
+	return (NULL);
+}
+
+static void	ft_splstr(char **ptr, char *str, char c, int n)
+{
+	int	i;
+	char	*aux;
 
 	i = 0;
-	word = malloc(len + 1);
-	if (!word)
-		return (NULL);
-	while (i < len)
+	if ((n > 0) && *str)
 	{
-		word[i] = s[i];
-		i++;
+		while (i < (n - 1))
+		{
+			aux = ft_strchr(str, c);
+			ptr[i++] = ft_substr(str, 0, aux - str);
+			if (ptr == 0)
+			{
+				ft_freeall(ptr, i);
+				return ;
+			}
+			while (*aux == c)
+				aux++;
+			str = aux;
+		}
+		ptr[i++] = ft_strdup(str);
+		ptr[i] = 0;
 	}
-	word[i] = '\0';
-	return (word);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**res;
-	int		i;
-	int		len;
+	int	n;
+	char	**ptr;
+	char	*str;
+	char	a[2];
 
-	i = 0;
-	if (!s)
-		return (NULL);
-	res = malloc((ft_count(s, c) + 1) * sizeof(char *));
-	if (!res)
-		return (NULL);
-	while (*s)
+	a[0] = c;
+	a[1] = '\0';
+	if (s)
 	{
-		while (*s == c)
-			s++;
-		len = 0;
-		while (s[len] && s[len] != c)
-			len++;
-		if (len > 0)
-			res[i++] = ft_dup(s, len);
-		s += len;
+		str = ft_strtrim(s, a);
+		if (str)
+		{
+			n = ft_count(str, c);
+			ptr = ft_calloc((n + 1), sizeof(char *));
+			if (ptr)
+				ft_splstr(ptr, str, c , n);
+			free(str);
+			return (ptr);
+		}
 	}
-	res[i] = NULL;
-	return (res);
+	return (0);
 }
-/*
+
+#include <string.h>
 #include <stdio.h>
 int	main(int argc, char *argv[])
 {
@@ -92,9 +114,13 @@ int	main(int argc, char *argv[])
 		while (result[i])
 		{
 			printf("objs memory alloc %s\n", result[i]);
-			free(result[i]);
 			i++;
 		}
 	}
+	char *token = strtok(argv[1], &argv[2][0]);
+	while (token != NULL) {
+        printf(" %s\n", token);
+       token = strtok(NULL, " - ");
+    }
 	return (0);
-}*/
+}
